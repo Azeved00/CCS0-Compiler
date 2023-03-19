ae365f7c4e05849ba9b9eb023ce52174264346c8
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2023-03-19 00:18:36 +0000

    states are no longer a list of atoms, now are a string

--- src/translator.erl
+++ src/translator.erl
@@ -1,19 +1,24 @@
 -module(translator).
--export([translate/1]).
+-export([start/0, translate/1]).
 
 % the ast of CCS0
--type ast() ::  {prefix, string(),ast()} |
+-type ast() ::  {prefix, atom(),ast()} |
                 {choise, ast(), ast()}  |
                 zero.
 
 % a transition from s to s' with action a
--type trans() :: {string(),string(),string()}.
+-type trans() :: {string(),atom(),string()}.
 % The Lts System
 -type lts() :: {[string()],[trans()],string()}.
 
-newState(A,State) -> [A] ++ [State].
 
-translate(zero) -> {['zero'],[],'zero'};
+start() ->
+    translate({prefix,'strike',{prefix, 'extinguish', zero}}).
+
+
+newState(A,State) -> atom_to_list(A) ++ "." ++ State.
+
+translate(zero) -> {["zero"],[],"zero"};
 translate({prefix, Action, Process}) -> 
     {States, Trans, Initial} = translate(Process),
     
