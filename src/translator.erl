13b8916c0d932b42c279078c60ff79dbcfa35e4e
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2023-03-21 09:16:42 +0000

    some bug fixes

--- src/translator.erl
+++ src/translator.erl
@@ -24,17 +24,17 @@ translate(Map, {prefix, Action, Process}) ->
     NTab =  updateTab(Tab,NState),
     State = maps:get(NState,NTab),
     
-    {NTab,[{State,Action,Initial}] ++ Trans, State};
+    {NTab,[{State,Action,Initial}] ++ Trans, {NState,State}};
 translate(Map, {choise, Process1, Process2}) -> 
-    {Tab1, Trans1, {Initial,_}} = translate(Map,Process1),
-    Tab1T = maps:remove(Tab1,Initial),
-    {Tab2, Trans2, {Initial,_}} = translate(Tab1T,Process2),
-    Tab2T = maps:remove(Tab2,Initial),
+    {Tab1, Trans1, {Initial1,_}} = translate(Map,Process1),
+    Tab1T = maps:remove(Initial1,Tab1),
+    {Tab2, Trans2, {Initial2,_}} = translate(Tab1T,Process2),
+    Tab2T = maps:remove(Initial2,Tab2),
     
     NState = {choise, Process1, Process2},
     NTab =  updateTab(Tab2T,NState),
     State = maps:get(NState,NTab),
     NTrans = alterTrans(State,Trans1) ++ alterTrans(State,Trans2),
 
-    {NTab, NTrans, State}.
+    {NTab, NTrans, {NState,State}}.
 
