93751cb66fc80f239aea5fd60108a9bf37c93f29
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2023-03-21 09:10:38 +0000

    now removing unused entries from table

--- src/translator.erl
+++ src/translator.erl
@@ -16,9 +16,9 @@ updateTab(Map, V) -> case maps:is_key(V, Map) of
                         end.
 
 % the translation logic 
-translate(Map, zero) -> {Map, [], zero};
+translate(Map, zero) -> {Map, [], {zero,zero}};
 translate(Map, {prefix, Action, Process}) -> 
-    {Tab, Trans, Initial} = translate(Map,Process),
+    {Tab, Trans, {_,Initial}} = translate(Map,Process),
     
     NState = {prefix, Action, Process},
     NTab =  updateTab(Tab,NState),
@@ -26,11 +26,13 @@ translate(Map, {prefix, Action, Process}) ->
     
     {NTab,[{State,Action,Initial}] ++ Trans, State};
 translate(Map, {choise, Process1, Process2}) -> 
-    {Tab1, Trans1, _} = translate(Map,Process1),
-    {Tab2, Trans2, _} = translate(Tab1,Process2),
+    {Tab1, Trans1, {Initial,_}} = translate(Map,Process1),
+    Tab1T = maps:remove(Tab1,Initial),
+    {Tab2, Trans2, {Initial,_}} = translate(Tab1T,Process2),
+    Tab2T = maps:remove(Tab2,Initial),
     
     NState = {choise, Process1, Process2},
-    NTab =  updateTab(Tab2,NState),
+    NTab =  updateTab(Tab2T,NState),
     State = maps:get(NState,NTab),
     NTrans = alterTrans(State,Trans1) ++ alterTrans(State,Trans2),
 
